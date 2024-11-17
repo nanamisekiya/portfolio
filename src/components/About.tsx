07f7d514a0cee9d0ab6b05ac6b3b44336583ede6
nanamisekiya <na511mi1009.3chigai@gmail.com>
2024-11-18 00:10:49 +0900

    about部分追加

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -4,8 +4,19 @@ import './About.css';
 const About: React.FC = () => {
   return (
     <section id="about">
-      <h2>About Me</h2>
-      <p>ここに自己紹介文を入力します。</p>
+      <div className="about-container">
+        <img
+          src="src/assets/girl.png"
+          alt="About me illustration"
+          className="about-image"
+        />
+        <div className="about-text">
+          <h2>About Me</h2>
+          <p>
+            自己紹介追加
+          </p>
+        </div>
+      </div>
     </section>
   );
 };
